5eab0aa2c5740a4e43866af0ac4fcc2869a7761f
Brian M. Anderson <brian.michael.anderson@gmail.com>
2019-06-10 22:19:41 +1000

    Removed EXT Directory

--- xcode-build/src/ext/openexr/IlmBase/Half/PBRT-V3.build/Debug/Half.build/Script-7298A318AE1743C58C8E7810.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/andewx/Developer/Maketools/pbrt-v3/xcode-build/src/ext/openexr/IlmBase/Half -f /Users/andewx/Developer/Maketools/pbrt-v3/xcode-build/src/ext/openexr/IlmBase/Half/CMakeScripts/Half_cmakeRulesBuildPhase.make$CONFIGURATION all
